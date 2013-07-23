6860cda526797ed6a06f4ace902cdf846fa2b219
sajjankc <sajjan.rajan@gmail.com>
2013-07-23 21:25:31 +0545

    Initial Commit

---
+++ EasyScreenRecord/ESRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESRAppDelegate.h
+//  EasyScreenRecord
+//
+//  Created by Sajjan on 7/23/13.
+//  Copyright (c) 2013 sajjankc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EasyScreenRecord/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EasyScreenRecord
+//
+//  Created by Sajjan on 7/23/13.
+//  Copyright (c) 2013 sajjankc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ESRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESRAppDelegate class]));
+    }
+}
